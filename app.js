1564780c4251fd59db1126d7bed93b3b342d83b2
ILTYAR MEHMEDOV <65760935+ilti@users.noreply.github.com>
2020-11-08 20:16:33 +0200

    Books JS file

--- app.js
+++ app.js
@@ -1,29 +1,140 @@
-function getInfo() {
-    let inputField = document.querySelector('#stopId');
-    let validBuses = [`1287`, `1308`, `1327`, `2334`]
-    let stopInfo = document.querySelector('#stopName')
-    let bussesList = document.querySelector(`#buses`);//ul
-    bussesList.innerHTML='' //to clear ul if already populated
+let loadBtn = document.querySelector('#loadBooks');
+let tbody = document.querySelector("body > table > tbody");
+let submitBtn = document.querySelector('#submit');
+let titleField = document.querySelector('#title');
+let authorField = document.querySelector('#author');
+let isbnField = document.querySelector('#isbn');
+let formEl = document.querySelector("body > form");
 
-    let url = `https://judgetests.firebaseio.com/businfo/${inputField.value}.json `
+submitBtn.addEventListener('click', submitFunction);
 
-    if (!validBuses.includes(inputField.value)) {
-        stopInfo.innerHTML = 'Error';
-        return
-    }
+loadBtn.addEventListener('click', loadFunction);
 
+function loadFunction() {
+    let url = 'https://books-exercise-23750.firebaseio.com/Books/.json';
     fetch(url)
-        .then((response) => response.json())
-        .then((data)=>{
-            stopInfo.innerHTML=data.name
-
-            let workArr = Object.entries(data.buses)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            tbody.innerHTML = ''
+            let workArr = Object.entries(data)
 
             for (const kvp of workArr) {
-                let li = document.createElement('li')
-                li.innerHTML=`Bus ${kvp[0]} arrives in ${kvp[1]} minutes`
-                bussesList.appendChild(li)     
+                console.log(workArr);
+                let trEl = document.createElement('tr');
+                trEl.setAttribute('data-id', `${kvp[0]}`)
+
+                tbody.appendChild(trEl)
+
+                let result =
+                    `<td>${kvp[1].title}</td>` +
+                    `<td>${kvp[1].author}</td>` +
+                    `<td>${kvp[1].isbn}</td>`
+
+                trEl.insertAdjacentHTML('beforeEnd', result)
+                let newTdEl = document.createElement('td');
+                let edtiBtn = document.createElement('button');
+                edtiBtn.innerHTML = 'Edit';
+
+                let deleteBtn = document.createElement('button');
+                deleteBtn.innerHTML = 'Delete'
+
+                trEl.appendChild(newTdEl);
+                newTdEl.appendChild(edtiBtn);
+                newTdEl.appendChild(deleteBtn)
+
+                deleteBtn.addEventListener('click', deleteFunction);
+                edtiBtn.addEventListener('click', editFUnction);
             }
         })
-        inputField.value=''
+}
+
+function deleteFunction(e) {
+    e.preventDefault();
+    let parent = e.target.parentElement.parentElement
+    let currentID = parent.getAttribute('data-id');
+
+    let url = `https://books-exercise-23750.firebaseio.com/Books/${currentID}.json`
+
+    let deleteWait = fetch(url, {
+        method: 'DELETE',
+    })
+
+    Promise.all([deleteWait])
+        .then(data => {
+            loadFunction()
+            alert('Success!')
+        })
+}
+
+function editFUnction(e) {
+    e.preventDefault();
+
+    let parent = e.target.parentElement.parentElement
+
+    let newDiv = document.createElement('div');
+    formEl.appendChild(newDiv)
+    let h3El = document.createElement('h3');
+    h3El.innerHTML = 'NEW FORM'
+    newDiv.appendChild(h3El)
+
+    let inputTitle = document.createElement('input');
+    inputTitle.value = `${parent.getElementsByTagName('td')[0].innerHTML}`;
+    
+    newDiv.appendChild(inputTitle)
+
+    let inoutAuthor = document.createElement('input');
+    inoutAuthor.value = `${parent.getElementsByTagName('td')[1].innerHTML}`;
+    newDiv.appendChild(inoutAuthor)
+
+    let inputIsbn = document.createElement('input');
+    inputIsbn.value = `${parent.getElementsByTagName('td')[2].innerHTML}`;
+    newDiv.appendChild(inputIsbn)
+
+    let confirmBtn = document.createElement('button')
+    confirmBtn.innerHTML = 'CONFIRM'
+    newDiv.appendChild(confirmBtn)
+
+    let newparent = e.target.parentElement.parentElement
+    let currentID = newparent.getAttribute('data-id')
+    
+    confirmBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        let url = `https://books-exercise-23750.firebaseio.com/Books/${currentID}.json`
+        let confWait = fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify({
+                'title': inputTitle.value,
+                'author': inoutAuthor.value,
+                'isbn': inputIsbn.value
+            })
+        })
+        newDiv.style.display='none'
+        Promise.all([confWait])
+            .then(data=>{
+                loadFunction()
+                alert('Success')
+            })
+
+    })
+}
+
+function submitFunction(e) {
+    e.preventDefault()
+    let url = 'https://books-exercise-23750.firebaseio.com/Books/.json';
+    let submitWait = fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            'title': titleField.value,
+            'author': authorField.value,
+            'isbn': isbnField.value
+        })      
+    })
+    Promise.all([submitWait])
+    .then(data=>{
+        loadFunction()
+    })
+    titleField.value = '';
+    authorField.value = '';
+    isbnField.value = '';
 }
